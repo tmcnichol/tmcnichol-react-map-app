b60b6bc4cafae4028cad1d1d72b2d151d1d2fb8c
tmcnichol <tlmcnichol@gmail.com>
2018-10-20 14:11:29 -0600

    Initialized map/markers/locations

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MapComponent from './MapComponent'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { GoogleApiWrapper } from 'google-maps-react';
 
 class App extends Component {
   render() {
@@ -10,9 +10,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2 className="App-title">Tom's Neighborhood Map</h2>
+          <h2 className="App-title">Boulder Local Guides</h2>
         </header>
-        <MapComponent google={this.props.google} />
+        <main>
+          <MapComponent google={this.props.google} />
+        </main>
+
       </div>
     );
   }
--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,23 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import ReactDOM from 'react-dom'
 
 class MapComponent extends Component {
+
+  state = {
+    locations: [
+      {name: "Google Boulder", location: {lat: 40.022227, lng: -105.254886}},
+      {name: "Library", location: {lat: 40.014166, lng: -105.281780}},
+      {name: "CU Boulder", location: {lat: 40.012438, lng: -105.270268}},
+      {name: "Boulder Theater", location: {lat: 40.019477, lng: -105.277287}},
+      {name: "BRC", location: {lat: 40.026080, lng: -105.256750}},
+      {name: "Yoga Pod Boulder", location: {lat: 40.020616, lng: -105.253073}},
+      {name: "Folsom Field", location: {lat: 40.009729, lng: -105.266926}},
+      {name: "Community Cycles", location: {lat: 40.025825, lng: -105.248474}},
+      {name: "Chautauqua", location: {lat: 39.999463, lng: -105.281478}},
+      {name: "Bartaco", location: {lat: 40.017378, lng: -105.282086}},
+      {name: "Sweet Cow", location: {lat: 40.025096, lng: -105.281170}},
+      {name: "Barnsley Tire", location: {lat: 40.022023, lng: -105.262315}},
+      {name: "Valmont Disc Golf", location: {lat: 40.028356, lng: -105.237393}}
+    ],
+    markers: []
+  }
+
+  componentDidMount() {
+    this.initMap()
+  }
+
+  initMap() {
+    const {google} = this.props
+    const bounds = new google.maps.LatLngBounds() //builder to create a min bound based on set of LatLng points
+    this.map = new google.maps.Map(ReactDOM.findDOMNode(this.refs.map), Object.assign({}, {
+      center: {lat: 45.188529, lng: 5.724523999999974},
+      zoom: 12,
+      mapTypeId: 'hybrid'
+    }))
+    this.state.locations.forEach((location, ind) => {
+      const marker = new google.maps.Marker({
+        position: {lat: location.location.lat, lng: location.location.lng},
+        map: this.map,
+        title: location.name
+      })
+      this.setState((state) => ({
+        markers: [...state.markers, marker]
+      }))
+      bounds.extend(marker.position)
+    })
+    this.map.fitBounds(bounds)
+  }
+
   render() {
+    const {locations, markers} = this.state
+    locations.forEach((loc) => {
+      if (markers[loc]) {
+        markers[loc].setVisible(true)
+      }
+    })
+
     return (
-      <div className="component">
-
-        <Map google={this.props.google} zoom={14}>
-
-          <Marker onClick={this.onMarkerClick}
-                  name={'Current location'} />
-
-          <InfoWindow onClose={this.onInfoWindowClose}>
-
-          </InfoWindow>
-        </Map>
+      <div>
+        <div className="container">
+          <ul className="locations-list">{
+            markers.filter(marker => marker.getVisible()).map((marker, key) =>
+            (<li key={key}>{marker.title}</li>))
+          }</ul>
+          <div role="application" className="map" ref="map"/>
+        </div>
       </div>
-    );
+    )
   }
 }
 
